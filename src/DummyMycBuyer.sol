16bda96fc58316ddf4d08b9dd33b8d1fcb5897f3
CalabashSquash <my_name_uhh@protonmail.com>
2022-08-24 15:01:04 +1000

    Remove buyMyc param and add payable value

--- src/DummyMycBuyer.sol
+++ src/DummyMycBuyer.sol
@@ -10,10 +10,23 @@ import {IMycBuyer} from "interfaces/IMycBuyer.sol";
 contract DummyMycBuyer is IMycBuyer {
     // 1 ETH = `exchangeRate` MYC
     uint256 public exchangeRate = 10000;
+    ERC20 public myc;
 
+    constructor(address _myc) {
+        myc = ERC20(_myc);
+    }
+
+    /**
+     * @notice JUST A DUMMY FUNCTION -> not a real implementation.
+     */
     function buyMyc(bytes calldata data)
         external
         payable
-        returns (uint256 mycOut)
-    {}
+        returns (uint256 )
+    {
+        uint256 mycOut = msg.value * exchangeRate;
+        require(myc.balanceOf(address(this)) >= mycOut, "Not enough balance");
+        mycBalance.transfer(msg.sender, mycOut);
+        return mycOut
+    }
 }
