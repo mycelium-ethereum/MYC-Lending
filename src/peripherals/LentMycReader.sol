8f780ff1472487d4454d797e2767e07934e23666
Dospore <tetthera@gmail.com>
2022-10-06 12:46:55 +1100

    Add natspec

--- src/peripherals/LentMycReader.sol
+++ src/peripherals/LentMycReader.sol
@@ -16,11 +16,16 @@ contract LentMycReader {
         uint256 totalAssets;
     }
 
-    /// get the current cycles apr
+    /// @notice External function to access internal _getCurrentCycleInfo
     function getCurrentCycleInfo(address lentMyc_, uint256 ethRewards) external view returns (uint256[] memory) {
         return _getCurrentCycleInfo(lentMyc_, ethRewards);
     }
 
+    /// @notice Get the current cycles reward reward and info given an eth reward amount
+    /// @dev Dospore
+    /// @param lentMyc_ address of lentMyc
+    /// @param ethRewards amount of eth that will be rewarded to current cycle stakers
+    /// @return Current cycles reward info based on the given eth rewards amount
     function _getCurrentCycleInfo(address lentMyc_, uint256 ethRewards) internal view returns (uint256[] memory) {
         ILentMyc lentMyc = ILentMyc(lentMyc_);
 
@@ -41,14 +46,16 @@ contract LentMycReader {
     }
 
 
-
-    /// Fetches
+    /// @notice Get an arbitrary cycles info
+    /// @dev Dospore
+    /// @param lentMyc_ address of lentMyc
+    /// @param cycle number to retrieve info on
+    /// @return Given cycles reward info or the currentCycles info based on the previous cycle
     function getCycleInfo(address lentMyc_, uint256 cycle) external view returns (uint256[] memory) {
         ILentMyc lentMyc = ILentMyc(lentMyc_);
         uint256 cycle_ = lentMyc.cycle();
         bool fetchingCurrentCycle = false;
         if (cycle >= cycle_) {
-            // set to cycle_ - 1
             cycle = cycle_ - 1;
             fetchingCurrentCycle = true;
         }
